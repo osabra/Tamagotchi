33c54a5bcac78ae5f6424c1719a114475fd83177
Oscar <arezvo92@gmail.com>
2026-09-20 23:59:08 +0200

    Fix Canvas transform import for animated ears

--- app/src/main/java/com/osabra/tamagotchi/MainActivity.kt
+++ app/src/main/java/com/osabra/tamagotchi/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
